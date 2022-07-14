7fd721c7767643482ad4ed7d8861d838ecbd7817
klivoi <klivoi@cwsafrica.org>
2022-07-14 07:56:18 +0300

    Add project files.

---
+++ EmployeeHierarchy/CustomException/ManagerMoreThanOne.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace EmployeeHierarchy.CustomException
+{
+    public class ManagerMoreThanOne : Exception
+    {
+        public ManagerMoreThanOne(string message) : base(message)
+        {
+        }
+    }
+}
